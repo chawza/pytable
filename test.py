243c0558288732affad9e3cdc922f2b57ea4d981
Nabeel KM <nabeelkahlil403@gmail.com>
2024-06-21 14:41:20 +0700

    excel test

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from pytable import Table
 from pydantic import BaseModel, Field
 from unittest import TestCase, main
 from typing import Optional
+from csv import reader
 
 class Model(BaseModel):
     title: str = Field(title='Title')
@@ -10,11 +11,9 @@ class Model(BaseModel):
 class TestTableCase(TestCase):
 
     def test_csv(self):
-
         table = Table(Model, initial=[Model(title='Test1', notes='Note 1'), Model(title='Test2', notes=None)])
         csv = table.to_csv()
 
-        from csv import reader
         csv_reader = reader(csv.getvalue().strip().split('\n'))
 
         rows = list(csv_reader) 
@@ -22,6 +21,18 @@ class TestTableCase(TestCase):
         self.assertEqual(len(rows), 3, rows)
 
         self.assertEqual(rows[0], ['Title', 'notes'])
+
+    def test_excel(self):
+        table = Table(Model, initial=[Model(title='Test1', notes='Note 1'), Model(title='Test2', notes=None)])
+        excel = table.to_excel_workbook()
+
+        sheet = excel.worksheets[0]
+        rows = list(sheet.iter_rows())
+        
+        self.assertEqual(len(rows), 3, rows)
+        values = [cell.value for cell in rows[0]]
+        self.assertEqual(values, ['Title', 'notes'])
+
 
 if __name__ == '__main__':
     main()
